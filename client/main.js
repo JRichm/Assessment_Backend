e9beece26f00c909456c06d31ede55f5e965e818
JRichm <jblackenmusic@gmail.com>
2023-03-10 16:01:26 -0700

    added assessment

--- client/main.js
+++ client/main.js
@@ -1,6 +1,16 @@
 const complimentBtn = document.getElementById("complimentButton")
 const fortuneBtn = document.getElementById("fortuneButton");
 
+const newGoalForm = document.getElementById('newGoalForm');
+
+const viewGoalTitle = document.getElementById('viewGoalTitle');
+const viewGoalData = document.getElementById('viewGoalData');
+const editGoalButton = document.getElementById('editGoalButton');
+const editGoalTitle = document.getElementById('editGoalTitle');
+const goalList = document.getElementById('goalsList');
+
+let editing = false;
+
 
 const getCompliment = () => {
     axios.get("http://localhost:4000/api/compliment/")
@@ -19,3 +29,141 @@ fortuneBtn.addEventListener('click', () => {
             alert(data);
         })
 });
+
+
+newGoalForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let body = {
+        goalTitle: document.getElementById('goalTitle').value,
+        goalData: document.getElementById('goalData').value
+    }
+
+    if (body.goalData === '' && body.goalTitle === '') {
+        alert('please enter goal\nempty fields: (title, goal)');
+        return;
+    } else if (body.goalData === '') {
+        alert('please enter goal\nempty fields: (goal)');
+        return;
+    } else if (body.goalTitle === '') {
+        alert('please enter goal\nempty fields: (title)');
+        return;
+    }
+
+    console.log(`\n( main.js ) body:`)
+    console.log(body)
+    axios.post('http://localhost:4000/api/goals/', body).then(res => {
+
+        while (goalList.firstChild) {
+            goalList.removeChild(goalList.firstChild);
+        }
+
+        let newGoalButton = document.createElement('div');
+        let newGoalButtonText = document.createElement('p');
+
+        newGoalButtonText.textContent = ' + New Goal';
+        newGoalButton.appendChild(newGoalButtonText);
+
+        newGoalButton.id = 'goal';
+
+        newGoalButton.addEventListener('click', newGoalButtonCallback)
+
+        goalList.appendChild(newGoalButton);
+
+        for (let i = 0; i < res.data.length; i++) {
+            let goalButton = document.createElement('div');
+            let goalButtonText = document.createElement('p');
+            let goalDeleteButton = document.createElement('button');
+
+            goalButtonText.textContent = res.data[i].goalTitle;
+            goalButton.appendChild(goalButtonText);
+
+            goalButton.id = 'goal';
+
+            goalButton.addEventListener('click', buttonClickCallback);
+
+            goalDeleteButton.id = "goalDeleteButton"
+            goalDeleteButton.textContent = 'X'
+            goalDeleteButton.addEventListener('click', deleteButtonCallback);
+
+            goalButton.appendChild(goalDeleteButton);
+            goalList.appendChild(goalButton);
+        }
+
+        console.log(res.data)
+
+    });
+});
+
+
+function buttonClickCallback(e) {
+    document.getElementById('newGoal').classList.add('hidden');
+    document.getElementById('viewGoal').classList.remove('hidden');
+    axios.get('http://localhost:4000/api/goals?goalTitle=' + this.textContent)
+    .then((result) => {
+        viewGoalTitle.textContent = result.data.goalTitle;
+        viewGoalData.textContent = result.data.goalData;
+    }).catch((err) => {
+        console.log(err);
+    })
+}
+
+function deleteButtonCallback(e) {
+    console.log(`\ndelete button parent content`);
+    console.log(this.parentElement);
+    axios.delete('http://localhost:4000/api/goals?goalTitle=' + this.parentElement.textContent)
+    .then((result) => {
+        console.log(`\nresult`);
+        console.log(result);
+
+        while (goalList.firstChild) {
+            goalList.removeChild(goalList.firstChild);
+        }
+
+        let newGoalButton = document.createElement('div');
+        let newGoalButtonText = document.createElement('p');
+
+        newGoalButtonText.textContent = ' + New Goal';
+        newGoalButton.appendChild(newGoalButtonText);
+
+        newGoalButton.id = 'goal';
+
+        newGoalButton.addEventListener('click', newGoalButtonCallback)
+
+        goalList.appendChild(newGoalButton);
+
+        for (let i = 0; i < result.data.length; i++) {
+            let goalButton = document.createElement('div');
+            let goalButtonText = document.createElement('p');
+            let goalDeleteButton = document.createElement('button');
+
+            goalButtonText.textContent = result.data[i].goalTitle;
+            goalButton.appendChild(goalButtonText);
+
+            goalButton.id = 'goal';
+
+            goalButton.addEventListener('click', buttonClickCallback);
+
+            goalDeleteButton.id = "goalDeleteButton"
+            goalDeleteButton.textContent = 'X'
+            goalDeleteButton.addEventListener('click', deleteButtonCallback);
+
+            goalButton.appendChild(goalDeleteButton);
+            goalList.appendChild(goalButton);
+        }
+        
+        console.log(`\nhide viewGoal`);
+
+        console.log(viewGoalTitle, this.parentElement.textContent)
+        if (viewGoalTitle.textContent + 'X' === this.parentElement.textContent) {
+            document.getElementById('viewGoal').classList.add('hidden');
+            document.getElementById('newGoal').classList.remove('hidden');
+        }
+    })
+    .catch((err) => console.log(err));
+}
+
+function newGoalButtonCallback() {
+    document.getElementById('viewGoal').classList.add('hidden');
+    document.getElementById('newGoal').classList.remove('hidden');
+}
--- server/index.js
+++ server/index.js
@@ -7,10 +7,81 @@ app.use(cors());
 
 app.use(express.json());
 
+let goalDB = [];
+
 const { getCompliment, getFortune } = require('./controller')
 
 app.get("/api/compliment", getCompliment);
 
 app.get("/api/fortune/", getFortune);
+
+app.post("/api/goals", (req, res) => {
+    let goalTitle = req.body.goalTitle;
+
+    let index;
+
+    for(let i = 0; i < goalDB.length; i++) {
+        let currentTitle = goalDB[i].goalTitle;
+        if (currentTitle === goalTitle) {
+            index = i;
+        }
+    }
+
+    if (index === undefined) {
+        goalDB.push(req.body);
+        res.status(200).send(goalDB);
+    } else {
+        res.status(400).send('goal already tracked')
+    }
+
+    
+});
+
+app.get('/api/goals', (req, res) => {
+    let goalTitle = req.query.goalTitle.split('')
+    let index;
+
+    goalTitle.splice(goalTitle.length - 1, 1);
+    goalTitle = goalTitle.join('');
+
+    for(let i = 0; i < goalDB.length; i++) {
+        let currentTitle = goalDB[i].goalTitle;
+        if (currentTitle === goalTitle) {
+            index = i;
+        }
+    }
+
+    if (index === undefined) {
+        res.status(400).send('goal not found');
+    } else {
+        res.status(200).send(goalDB[index]);
+    }
+
+});
+
+app.delete('/api/goals', (req,res) => {
+    console.log(`\ndelete method called`);
+    let goalTitle = req.query.goalTitle.split('')
+    let index;
+
+    goalTitle.splice(goalTitle.length - 1, 1);
+    goalTitle = goalTitle.join('');
+    console.log(goalTitle);
+
+    for(let i = 0; i < goalDB.length; i++) {
+        let currentTitle = goalDB[i].goalTitle;
+        if (currentTitle === goalTitle) {
+            index = i;
+        }
+    }
+
+    if (index === undefined) {
+        res.status(400).send('goal not found');
+    } else {
+        goalDB.splice(index, 1);
+        res.status(200).send(goalDB);
+    }
+});
+
 
 app.listen(4000, () => console.log("Server running on 4000"));
